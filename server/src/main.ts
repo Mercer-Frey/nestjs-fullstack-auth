a9b4dc091d84efbd19df564d8d4690f73a1d2778
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-21 10:08:04 +0200

    installing db

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+	const config = app.get(ConfigService)
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true
+		})
+	)
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposeHeaders: ['set-cookie']
+	})
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
 }
-bootstrap();
+bootstrap()
---
+++ server/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PrismaClient } from '@prisma/__generated__/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+@Injectable()
+export class PrismaService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
+	constructor(private readonly configService: ConfigService) {
+		const adapter = new PrismaPg({
+			connectionString: configService.getOrThrow<string>('POSTGRES_URI')
+		})
+		super({ adapter })
+	}
+
+	async onModuleInit() {
+		await this.$connect()
+	}
+
+	async onModuleDestroy() {
+		await this.$disconnect()
+	}
+}
