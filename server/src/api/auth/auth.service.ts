c9c73c37d22a823611654beeb68b23a3798255af
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-31 01:43:05 +0200

    two-factor auth

--- server/src/api/auth/auth.service.ts
+++ server/src/api/auth/auth.service.ts
@@ -88,7 +88,7 @@ export class AuthService {
 				}
 			}
 
-			this.twoFactorAuthService.validateTwoFactorToken(
+			await this.twoFactorAuthService.validateTwoFactorToken(
 				user.email,
 				dto.code
 			)
